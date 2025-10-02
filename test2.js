8afe1059e2b61efb3f4ae90f3715bd3c8f8adc2a
MARADENTRO <maradentro@duck.com>
2025-10-02 10:28:19 +0500

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,33 @@
+// bigText.js
+
+const bigText = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Sed non risus. Suspendisse lectus tortor, dignissim sit amet, 
+adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. 
+Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, 
+non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, 
+scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. 
+
+Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. 
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. 
+Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+Curabitur vitae magna. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. 
+Nulla facilisi. Etiam sit amet orci eget eros faucibus tincidunt. 
+Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. 
+Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero.
+`;
+
+// Вывод в консоль
+function printBigText() {
+  console.log(bigText);
+}
+
+// Экспорт для использования в других файлах
+module.exports = {
+  bigText,
+  printBigText
+};
+
